a7d4f5a66dc0374f53321e5b3fac992b2e37632b
luis95gr <36051490+luis95gr@users.noreply.github.com>
2018-02-02 23:39:56 -0600

    Initial commit

---
+++ app/src/main/java/com/pef/login/Fragments/recordsFragment.java
@@ -0,0 +1,19 @@
+package com.pef.login.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pef.login.R;
+
+
+public class recordsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_records, container, false);
+    }
+}
