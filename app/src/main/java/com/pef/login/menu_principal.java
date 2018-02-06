552420169449ecb4a8764a9a3b0eb42feb2904a8
luis95gr <36051490+luis95gr@users.noreply.github.com>
2018-02-05 18:08:44 -0600

    Initial commit

--- app/src/main/java/com/pef/login/menu_principal.java
+++ app/src/main/java/com/pef/login/menu_principal.java
@@ -1,5 +1,6 @@
 package com.pef.login;
 
+import android.graphics.Color;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -11,8 +12,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class menu_principal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView day;
+    TextView text_monday;
+    TextView text_tuesday;
+    TextView text_wednesday;
+    TextView text_thursday;
+    TextView text_friday;
+    TextView text_saturday;
+    TextView text_sunday;
+    SimpleDateFormat dia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,60 @@ public class menu_principal extends AppCompatActivity implements NavigationView.
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        //Variables
+        day = (TextView)findViewById(R.id.textView4);
+        text_monday = (TextView)findViewById(R.id.text_monday);
+        text_tuesday = (TextView)findViewById(R.id.text_tuesday);
+        text_wednesday = (TextView)findViewById(R.id.text_wednesday);
+        text_thursday = (TextView)findViewById(R.id.text_thursday);
+        text_friday = (TextView)findViewById(R.id.text_friday);
+        text_saturday = (TextView)findViewById(R.id.text_saturday);
+        text_sunday = (TextView)findViewById(R.id.text_sunday);
+
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        String currentDateTimeStrin = formatter.format(today);
+
+        if (currentDateTimeStrin.equalsIgnoreCase("Monday") || currentDateTimeStrin.equalsIgnoreCase("Lunes")){
+            text_monday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else{
+            text_monday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        if (currentDateTimeStrin.equalsIgnoreCase("Tuesday") || currentDateTimeStrin.equalsIgnoreCase("Martes")){
+            text_tuesday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else{
+            text_tuesday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        if (currentDateTimeStrin.equalsIgnoreCase("Wednesday") || currentDateTimeStrin.equalsIgnoreCase("Miércoles")){
+            text_wednesday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else {
+            text_wednesday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        if (currentDateTimeStrin.equalsIgnoreCase("Thursday") || currentDateTimeStrin.equalsIgnoreCase("Jueves")){
+            text_thursday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else {
+            text_thursday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        if (currentDateTimeStrin.equalsIgnoreCase("Friday") || currentDateTimeStrin.equalsIgnoreCase("Viernes")){
+            text_friday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else {
+            text_friday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        if (currentDateTimeStrin.equalsIgnoreCase("Saturday") || currentDateTimeStrin.equalsIgnoreCase("Sabado")){
+            text_saturday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else {
+            text_saturday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        if (currentDateTimeStrin.equalsIgnoreCase("Sunday") || currentDateTimeStrin.equalsIgnoreCase("Domingo")){
+            text_sunday.setBackgroundColor(Color.parseColor("#b8ddcd"));
+        }else {
+            text_sunday.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
