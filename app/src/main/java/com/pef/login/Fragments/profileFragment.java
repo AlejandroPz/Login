d2900742d819fd103acccc3f26525e37fa9ac6d7
luis95gr <36051490+luis95gr@users.noreply.github.com>
2018-02-08 20:02:25 -0600

    Initial commit

--- app/src/main/java/com/pef/login/Fragments/profileFragment.java
+++ app/src/main/java/com/pef/login/Fragments/profileFragment.java
@@ -6,15 +6,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.pef.login.R;
 //jhaksn
 
 public class profileFragment extends Fragment {
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
     }
 }
